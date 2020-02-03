56832c97d67eccf21779c642020947bfa40a8f22
No-YE <nye7181@gmail.com>
2020-02-03 13:52:26 +0900

    google env load function lint에 맞도록 수정

--- server/src/infrastructure/constant/env.ts
+++ server/src/infrastructure/constant/env.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import _ from 'lodash'
+['GOOGLE_CLIENT_ID', 'GOOGLE_CLIENT_SECRET', 'GOOGLE_REDIRECT_URI'])['GOOGLE_CLIENT_ID', 'GOOGLE_CLIENT_SECRET', 'GOOGLE_REDIRECT_URI'])import _ from 'lodash';
 import { Either, left, right } from '../lib/either';
 
 function checkEnv(env: string): boolean {
@@ -12,8 +12,8 @@ export function getGoogle(): Either<Error, object> {
   const envs = ['GOOGLE_CLIENT_ID', 'GOOGLE_CLIENT_SECRET', 'GOOGLE_REDIRECT_URI'];
   const filtedEnvs = _
     .chain(['GOOGLE_CLIENT_ID', 'GOOGLE_CLIENT_SECRET', 'GOOGLE_REDIRECT_URI'])
-    .map(env => checkEnv(env))
-    .filter(env => env)
+    .map((env) => checkEnv(env))
+    .filter((env) => env)
     .value();
 
   return envs.length !== filtedEnvs.length ? left(new Error()) : right({
