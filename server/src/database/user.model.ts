aea972ec8dad95319f79253e71754d40f6f7fcb5
No-YE <nye7181@gmail.com>
2020-02-12 18:23:39 +0900

    partial로 attribute를 optional하게 변경

---
+++ server/src/database/user.model.ts
@@ -0,0 +1,42 @@
+import {
+  DataTypes, Model, BuildOptions, ModelAttributeColumnOptions, AbstractDataTypeConstructor, AbstractDataType,
+} from 'sequelize';
+import db from './db';
+import { User } from '~/data/entity';
+
+type UserModel = User & typeof Model;
+type UserModelStatic = typeof Model & {
+  new (values?: object, options?: BuildOptions): UserModel;  
+};
+type UserModelAttributes = {
+  [name in (keyof User)]: ModelAttributeColumnOptions | string | AbstractDataTypeConstructor | AbstractDataType;
+};
+
+const userAttribute: Partial<UserModelAttributes> = {
+  id: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM('admin', 'operator', 'noRole'),
+    defaultValue: 'noRole',
+  },
+  socialId: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    allowNull: false,
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+};
+
+export const UserModel = <UserModelStatic>db.define('Users', userAttribute as UserModelAttributes);
