72073a5d7bc6a700b88fcbb48942572c2f6e58f3
No-YE <nye7181@gmail.com>
2020-02-13 11:03:40 +0900

    env 추가

--- server/src/constant/env.ts
+++ server/src/constant/env.ts
@@ -7,26 +7,42 @@ export type GoogleEnv = {
   redirectUri: string;
 };
 
+export type MysqlEnv = {
+  host: string;
+  user: string;
+  port: string;
+  password: string;
+};
+
 function getEnv(key: string): TaskEither<Error, string> {
   const env = process.env[key];
   return env === undefined ? left(new Error()) : right(env);
 }
 
-const combineGoogleEnv = ((clientId: string) => {
-  return ((clientSecret: string) => {
-    return ((redirectUri: string) => {
-      return {
-        clientId,
-        clientSecret,
-        redirectUri,
-      };
-    });
-  });
-});
+const combineGoogleEnv = ((clientId: string) => ((clientSecret: string) => ((redirectUri: string) => ({
+  clientId,
+  clientSecret,
+  redirectUri,
+}))));
+
+const combineMysqlEnv = ((host: string) => ((user: string) => ((port: string) => ((password: string) => ({
+  host,
+  user,
+  port,
+  password,
+})))));
 
 export function getGoogleEnv(): TaskEither<Error, GoogleEnv> {
   return ap(getEnv('GOOGLE_REDIRECT_URI'))
     (ap(getEnv('GOOGLE_CLIENT_SECRET'))
     (ap(getEnv('GOOGLE_CLIENT_ID'))
     (right(combineGoogleEnv))));
+}
+
+export function getMysqlEnv(): TaskEither<Error, MysqlEnv> {
+  return ap(getEnv('MYSQL_PASSWORD'))
+    (ap(getEnv('MYSQL_PORT'))
+    (ap(getEnv('MYSQL_USER'))
+    (ap(getEnv('MYSQL_HOST'))
+    (right(combineMysqlEnv)))));
 }
