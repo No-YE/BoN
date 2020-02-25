b1b4a893020676a57170177134609a64de80ef87
No-YE <nye7181@gmail.com>
2020-02-25 12:47:38 +0900

    postImage entity 작성

---
+++ src/database/entity/post-image.entity.ts
@@ -0,0 +1,43 @@
+/*eslint-disable @typescript-eslint/no-unused-vars*/
+import {
+  Entity, PrimaryGeneratedColumn, Column, ManyToOne, BeforeUpdate, BeforeInsert,
+} from 'typeorm';
+import Post from './post.entity';
+
+@Entity()
+export default class PostImage {
+  static of(postImage: Partial<PostImage>): PostImage {
+    return new this(postImage);
+  }
+
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column()
+  uri?: string;
+
+  @ManyToOne((_) => Post)
+  post?: Post;
+
+  @Column()
+  createdAt?: Date;
+
+  @Column()
+  updatedAt?: Date;
+
+  @BeforeInsert()
+  updateDateCreation(): void {
+    const currentDate = new Date();
+    this.createdAt = currentDate;
+    this.updatedAt = currentDate;
+  }
+
+  @BeforeUpdate()
+  updateDateUpdate(): void {
+    this.updatedAt = new Date();
+  }
+
+  constructor(postImage: Partial<PostImage>) {
+    Object.assign(this, postImage);
+  }
+}
