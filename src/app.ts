43695d4fd7081351973b462f70d0b9c181e89c11
No-YE <nye7181@gmail.com>
2020-02-27 21:36:37 +0900

    app.ts에 rest router 추가

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, {
 } from 'express';
 import next from 'next';
 import path from 'path';
+import router from './presenter/rest';
 
 const dev = process.env.NODE_ENV !== 'production';
 const dir = path.resolve(__dirname, './webview');
@@ -15,15 +16,16 @@ async function createApp(): Promise<void> {
 
   const handle = nextApp.getRequestHandler();
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.all('*', (req: Request, res: Response) => handle(req, res));
-
-  app.use((err: Error, req: Request, res: Response, nextF: NextFunction) => {
-    console.log(err);
-    res.send(err).end();
-    nextF();
-  });
+  app
+    .use(express.json())
+    .use('/api', router)
+    .use(express.urlencoded({ extended: true }))
+    .all('*', (req: Request, res: Response) => handle(req, res))
+    .use((err: Error, req: Request, res: Response, nextF: NextFunction): void => {
+      console.log(err);
+      res.send(err).end();
+      nextF();
+    });
 
   app.listen(4000, () => {
     console.log('server start');
