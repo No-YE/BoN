e4ee59e7dd87ad3fc71764171f9f0ae8271e4f68
No-YE <nye7181@gmail.com>
2020-03-02 15:26:13 +0900

    createPost axios 추가

--- src/webview/lib/api/post.ts
+++ src/webview/lib/api/post.ts
@@ -1,4 +1,5 @@
 import queryString from 'query-string';
+import { AxiosResponse } from 'axios';
 import axios from '../axios-client';
 
 type GetPostsPayload = {
@@ -6,7 +7,15 @@ type GetPostsPayload = {
   limit?: number;
 };
 
-export const getPosts = (payload: GetPostsPayload) => {
+export const getPosts = (payload: GetPostsPayload): Promise<AxiosResponse> => {
   const query = queryString.stringify(payload);
   return axios.get(`/post?${query}`);
 };
+
+type CreatePostPayload = {
+  title: string;
+  content: string;
+  categoryNames: Array<string>;
+};
+
+export const createPost = (payload: CreatePostPayload): Promise<AxiosResponse> => axios.post('/post', payload);
