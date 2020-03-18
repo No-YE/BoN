a617e17375e779f1ba0471f4e08689f24bb7f1e6
No-YE <nye7181@gmail.com>
2020-03-18 21:14:21 +0900

    api 주소 변경

--- src/webview/lib/axios-client.ts
+++ src/webview/lib/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
-  ? 'http://www.noye.xyz:3000/api/v1'
+  ? 'http://www.noye.xyz/api/v1'
   : 'http://localhost:3000/api/v1';
 
 export default axios.create({
