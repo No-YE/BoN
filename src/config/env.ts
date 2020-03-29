f0d86f73ce841f059f1900fa4c55a6a69f90c1c3
No-YE <nye7181@gmail.com>
2020-03-29 22:38:45 +0900

    env load를fp-ts ap에서 typejoi로 변경

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,14 +1,33 @@
 import 'dotenv/config';
-import {
-  Either, left, right, ap,
-} from 'fp-ts/lib/Either';
-import { pipe } from 'fp-ts/lib/pipeable';
+import { Either } from 'fp-ts/lib/Either';
+import Joi from 'typesafe-joi';
+import validate from '~/lib/validate';
 
 export type GoogleEnv = {
   clientId: string;
   clientSecret: string;
   redirectUri: string;
 };
+
+function googleEnvValidate(
+  obj: { [key in keyof GoogleEnv]: unknown },
+): Either<Error, GoogleEnv> {
+  const schema = Joi.object({
+    clientId: Joi.string().required(),
+    clientSecret: Joi.string().required(),
+    redirectUri: Joi.string().required(),
+  });
+
+  return validate<GoogleEnv>(schema, obj);
+}
+
+export function getGoogleEnv(): Either<Error, GoogleEnv> {
+  return googleEnvValidate({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    redirectUri: process.env.GOOGLE_REDIRECT_URI,
+  });
+}
 
 export type MysqlEnv = {
   nodeEnv: string;
@@ -19,72 +38,61 @@ export type MysqlEnv = {
   password: string;
 };
 
+function mysqlEnvValidate(
+  obj: { [key in keyof MysqlEnv]: unknown },
+): Either<Error, MysqlEnv> {
+  const schema = Joi.object({
+    nodeEnv: Joi.string().required(),
+    database: Joi.string().required(),
+    host: Joi.string().required(),
+    username: Joi.string().required(),
+    port: Joi.number().required(),
+    password: Joi.string().required(),
+  });
+
+  return validate<MysqlEnv>(schema, obj);
+}
+
+export function getMysqlEnv(): Either<Error, MysqlEnv> {
+  return mysqlEnvValidate({
+    nodeEnv: process.env.NODE_ENV,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    username: process.env.MYSQL_USERNAME,
+    port: process.env.MYSQL_PORT,
+    password: process.env.MYSQL_PASSWORD,
+  });
+}
+
 export type SentryEnv = {
   nodeEnv: string;
   dsn: string;
 };
 
-function getEnv(key: string): Either<Error, string> {
-  const env = process.env[key];
-  return env === undefined ? left(new Error()) : right(env);
-}
+function sentryEnvValidate(
+  obj: { [key in keyof SentryEnv]: unknown },
+): Either<Error, SentryEnv> {
+  const schema = Joi.object({
+    nodeEnv: Joi.string().required(),
+    dsn: Joi.string().required(),
+  });
 
-function getEnvAsNumber(key: string): Either<Error, number> {
-  const env = process.env[key];
-  const value = Number(env);
-  return Number.isNaN(value) ? left(new Error()) : right(value);
-}
-
-const combineGoogleEnv = (clientId: string) => (clientSecret: string) => (redirectUri: string): GoogleEnv => ({
-  clientId,
-  clientSecret,
-  redirectUri,
-});
-
-const combineMysqlEnv = (nodeEnv: string) => (database: string) => (host: string) => (username: string) => (
-  port: number,
-) => (
-  password: string,
-): MysqlEnv => ({
-  nodeEnv,
-  database,
-  host,
-  username,
-  port,
-  password,
-});
-
-const combineSentryEnv = (nodeEnv: string) => (dsn: string): SentryEnv => ({ nodeEnv, dsn });
-
-export function getGoogleEnv(): Either<Error, GoogleEnv> {
-  return pipe(
-    right(combineGoogleEnv),
-    ap(getEnv('GOOGLE_CLIENT_ID')),
-    ap(getEnv('GOOGLE_CLIENT_SECRET')),
-    ap(getEnv('GOOGLE_REDIRECT_URI')),
-  );
-}
-
-export function getMysqlEnv(): Either<Error, MysqlEnv> {
-  return pipe(
-    right(combineMysqlEnv),
-    ap(getEnv('NODE_ENV')),
-    ap(getEnv('MYSQL_DATABASE')),
-    ap(getEnv('MYSQL_HOST')),
-    ap(getEnv('MYSQL_USERNAME')),
-    ap(getEnvAsNumber('MYSQL_PORT')),
-    ap(getEnv('MYSQL_PASSWORD')),
-  );
+  return validate<SentryEnv>(schema, obj);
 }
 
 export function getSentryDsn(): Either<Error, SentryEnv> {
-  return pipe(
-    right(combineSentryEnv),
-    ap(getEnv('NODE_ENV')),
-    ap(getEnv('SENTRY_DSN')),
-  );
+  return sentryEnvValidate({
+    nodeEnv: process.env.NODE_ENV,
+    dsn: process.env.SENTRY_DSN,
+  });
+}
+
+function jwtEnvValidate(jwt?: string): Either<Error, string> {
+  const schema = Joi.string().required();
+
+  return validate<string>(schema, jwt as string);
 }
 
 export function getJwtSecret(): Either<Error, string> {
-  return getEnv('JWT_SECRET');
+  return jwtEnvValidate(process.env.JWT_SECRET);
 }
--- src/lib/validate.ts
+++ src/lib/validate.ts
@@ -1,10 +1,10 @@
 import { left, right, Either } from 'fp-ts/lib/Either';
-import { ObjectSchema, Value } from 'typesafe-joi';
+import { ObjectSchema, StringSchema } from 'typesafe-joi';
 import { Error400 } from '~/lib/error';
 
 export default <T>(
   //eslint-disable-next-line @typescript-eslint/no-explicit-any
-  schema: ObjectSchema<Value<Record<never, never>, any, never, undefined, never>>,
+  schema: ObjectSchema<any>| StringSchema<any>,
   obj: { [key in keyof T]: unknown },
 ): Either<Error, T> => {
   const result = schema.validate(obj);
