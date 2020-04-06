88dbb63eb9ea6fb2eeb2db12b4e2f44dcb1373be
No-YE <nye7181@gmail.com>
2020-04-06 22:48:55 +0900

    schema 위치 변경

---
+++ src/domain/aggregate/base.ts
@@ -0,0 +1,28 @@
+import { EntitySchemaColumnOptions } from 'typeorm';
+
+export type BaseEntity = {
+  id?: number;
+  isActive?: boolean;
+  createdAt?: Date;
+  updatedAt: Date;
+};
+
+export const baseSchema: { [id in keyof Required<BaseEntity>]: EntitySchemaColumnOptions } = {
+  id: {
+    type: 'bigint',
+    primary: true,
+    generated: true,
+  },
+  isActive: {
+    type: 'boolean',
+    default: true,
+  },
+  createdAt: {
+    type: 'timestamp with time zone',
+    createDate: true,
+  },
+  updatedAt: {
+    type: 'timestamp with time zone',
+    updateDate: true,
+  },
+};
